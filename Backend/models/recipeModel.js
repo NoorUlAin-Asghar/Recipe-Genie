008578a8a23e153dbd162481b3788a88366285ce
hhuma50 <humafatima38@gmail.com>
2025-05-04 04:59:23 -0700

    resolve conflict 1

--- Backend/models/recipeModel.js
+++ Backend/models/recipeModel.js
@@ -1,19 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   title: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  description: { type: String, required: true },
-  cookTime: { type: Number, required: true },
-  ingredients: [{
-    name: { type: String, required: true }
-  }],
-  instructions: { type: String, required: true },
-  likes: { type: Number, default: 0 },          // Total like count
-  likedBy: [{ type: Schema.Types.ObjectId, ref: 'User' }], // Array of user IDs
+  description: { type: String, required: true }, // Short overview or intro
+  cookTime: { type: Number, required: true },    // In minutes
+  serving:{type: Number, required:true},
+  ingredients: [
+    { type: String, required: true }
+  ],
+  instructions: [
+    { type: String, required: true }
+  ],
+  image: { type: String, default: '' },
+  likes:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
 }, { timestamps: true });
 
+
 const Recipe = mongoose.model('Recipe', recipeSchema);
+
 module.exports = Recipe;
